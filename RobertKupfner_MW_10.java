db689b0ddeb36b76b1a158cbba865555148730fb
Azuesflip <Azuesflip@gmail.com>
2013-11-25 10:14:57 -0700

    Beginning outline
    Published the beginning outline for our project.

---
+++ RobertKupfner_MW_10.java
@@ -0,0 +1,37 @@
+/*
+ * This program is used to take an input file and put the first 20
+ * items. Then it will put it into order from largest to smallest 
+ * while also outputing its ariginal position of how it's inputed.
+ * If the list is larger than 20 integers it will flush the rest
+ * to keep them from the array and prevvent bugs.
+ * Robert Kupfner
+ * Program #8, CS 1050, MW
+ * Java 1.7.0_25, Lenovo Ideapad Y400, Windows 8
+ */
+package robertkupfner_mw_10;
+
+
+public class RobertKupfner_MW_10 
+{
+
+    
+    public static void main(String[] args) 
+    {
+        
+    }
+    
+    public static int input_File(String fileLocation)
+    {
+        
+    }
+    
+    public static int[] inputArray(int numberInput)
+    {
+        
+    }
+    
+    public static int arraySorter (int[] inputArray)
+    {
+        
+    }
+}
