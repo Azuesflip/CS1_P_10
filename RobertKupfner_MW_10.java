d5954175d3d27ba1110e19a3f0767f749af79ffa
Azuesflip <Azuesflip@gmail.com>
2013-11-25 10:42:24 -0700

    New stuff
    Added new Methods and changed a few variable types to make things work
    better.

--- RobertKupfner_MW_10.java
+++ RobertKupfner_MW_10.java
@@ -10,6 +10,9 @@
  */
 package robertkupfner_mw_10;
 
+import java.io.*;
+import java.text.DecimalFormat;
+import java.util.Scanner;
 
 public class RobertKupfner_MW_10 
 {
@@ -20,12 +23,20 @@ public class RobertKupfner_MW_10
         
     }
     
-    public static int input_File(String fileLocation)
+    public static Scanner input_File(String fileLocation)
     {
         
     }
     
-    public static int[] inputArray(int numberInput)
+    public static int arrayCounter (Scanner counterInput)
+    {
+        while (counterInput.hasNext())
+        {
+            
+        }
+    }
+    
+    public static int[] inputArray (String numberInput)
     {
         
     }
@@ -34,4 +45,52 @@ public class RobertKupfner_MW_10
     {
         
     }
+    
+    public static String heading () 
+    {
+        
+    }
+    
+    public static String outputFormater()
+    {
+        
+    }
+            
+    // leftPad method
+    /**
+     *
+     * @param number
+     * @param width
+     * @param mask
+     * @param padding
+     * @return
+     */
+    
+    public static String leftPad (double number, int width, 
+                                 String mask, String padding)
+   {
+         final int DEFAULT_WIDTH = 8; // If 'width' is out of range
+         final int MIN_WIDTH = 3;     // Minimum width allowed
+         String strPad;               // String to be returned
+         int charsToPad;              // The number of charactes to pad
+
+         //  Convert number to a String
+         DecimalFormat fmt = new DecimalFormat(mask);
+         strPad = fmt.format(number);
+
+         // Use the default width if the width parameter is no good.
+         // Using the length of the String str, calculate
+         // the number of characters to pad on the left
+         if (width < MIN_WIDTH) width = DEFAULT_WIDTH;
+         charsToPad = width - strPad.length();
+
+         // Pad str by spaces on the left so that the resulting length 
+         // of str is 'width' characters
+         for (int i = 0;  i < charsToPad;  i++)
+         {
+               strPad = padding + strPad;
+         }
+         return strPad;
+   } // end leftPad()
+    
 }
